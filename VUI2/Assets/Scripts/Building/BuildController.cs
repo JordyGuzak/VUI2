f341236ecfa600d914fbe93f4ddce43aae0005bc
Jordy Guzak <j.guzak@student.fontys.nl>
2017-11-18 17:27:42 +0100

    Building

---
+++ VUI2/Assets/Scripts/Building/BuildController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildController : MonoBehaviour {
+
+    public float maxSnapDistance = 0.5f;
+    public GameObject[] buildPrefabs;
+
+    private GameObject objectInHand;
+    private SnapPointManager spmInHand;
+	
+	// Update is called once per frame
+	void Update () {
+        if (buildPrefabs.Length < 1) return;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SpawnObject(0);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Release();
+        }
+
+        if (objectInHand)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 50f, ~(1 << objectInHand.layer)))
+            {
+                Vector3 targetPos = hit.point;
+                Vector3 offset = spmInHand.SnapPoints[0].localPosition;
+                Transform nearestSnapPoint = null;
+
+                Collider[] hitColliders = Physics.OverlapSphere(targetPos, maxSnapDistance);
+                for (int i = 0; i < hitColliders.Length; i++)
+                {
+                    SnapPointManager spm = hitColliders[i].GetComponent<SnapPointManager>();
+                    if (spm && spm != spmInHand)
+                    {
+                        Transform snapPoint = spm.GetNearestSnapPoint(targetPos);
+                        float distance = Vector3.Distance(targetPos, snapPoint.position);
+
+                        if (distance <= maxSnapDistance)
+                        {
+                            if (nearestSnapPoint == null || Vector3.Distance(targetPos, nearestSnapPoint.position) > distance)
+                            {
+                                nearestSnapPoint = snapPoint;
+                            }
+                        }
+                    }
+                }
+
+                if (nearestSnapPoint)
+                {
+                    targetPos = nearestSnapPoint.position;
+                    offset = spmInHand.GetNearestSnapPoint(targetPos).localPosition;
+                }        
+
+                objectInHand.transform.position = targetPos - offset;
+            }
+        }
+	}
+
+    void SpawnObject(int index)
+    {
+        if (objectInHand)
+            Destroy(objectInHand);
+
+        objectInHand = Instantiate(buildPrefabs[index]);
+        SetLayerRecursively(objectInHand, LayerMask.NameToLayer("Building"));
+
+        spmInHand = objectInHand.GetComponent<SnapPointManager>();
+        if (spmInHand)
+        {
+            spmInHand.Init();
+        }
+    }
+
+    void Release()
+    {
+        if (!objectInHand) return;
+
+        SetLayerRecursively(objectInHand, 0);// Default layer
+        objectInHand = null;
+        spmInHand = null;
+    }
+
+    void SetLayerRecursively(GameObject obj, int layer)
+    {
+        obj.layer = layer;
+
+        foreach (Transform child in obj.transform)
+        {
+            SetLayerRecursively(child.gameObject, layer);
+        }
+    }
+}
---
+++ VUI2/Assets/SnapPointManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class SnapPointManager : MonoBehaviour {
+
+    [HideInInspector]
+    public Transform[] SnapPoints;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        SnapPoints = new Transform[transform.childCount];
+
+        for (int i = 0; i < SnapPoints.Length; i++)
+        {
+            SnapPoints[i] = transform.GetChild(i);
+        }
+    }
+
+    public Transform GetNearestSnapPoint(Vector3 pos)
+    {
+        float nearestDistance = Vector3.Distance(pos, SnapPoints[0].position);
+        int nearestIndex = 0;
+
+        for (int i = 1; i < SnapPoints.Length; i++)
+        {
+            var currDistance = Vector3.Distance(pos, SnapPoints[i].position);
+
+            if (currDistance < nearestDistance)
+            {
+                nearestDistance = currDistance;
+                nearestIndex = i;
+            }
+        }
+
+        return SnapPoints[nearestIndex];
+    }
+}
