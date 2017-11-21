3f1c8a2afd170d866ac928998a7e294f6cbae87e
Jordy Guzak <j.guzak@student.fontys.nl>
2017-11-21 18:18:32 +0100

    Start of inventory with VRTK

---
+++ VUI2/Assets/ToggleGameObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class ToggleGameObject : MonoBehaviour {
+	
+	public void ToggleActiveSelf()
+    {
+        transform.gameObject.SetActive(!transform.gameObject.activeSelf);
+    }
+}
