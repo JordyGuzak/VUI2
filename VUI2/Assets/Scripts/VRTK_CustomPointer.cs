020b479a90b4243cc856a9427e0b96e04998fc95
JordyGuzak1991 <j.guzak@student.fontys.nl>
2017-11-22 14:24:55 +0100

    added prefabs

---
+++ VUI2/Assets/Scripts/VRTK_CustomPointer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class VRTK_CustomPointer : VRTK.VRTK_Pointer {
+
+
+    public override void OnActivationButtonPressed(ControllerInteractionEventArgs e)
+    {
+        base.OnActivationButtonPressed(e);
+
+    }
+}
